75b3d39bc2b2522a89878221a8a3976abd37eb0a
ihuerta-PC\ihuerta <ivan.huerta.h@gmail.com>
2017-05-19 11:35:39 -0400

    commit

--- DataManagement/Controllers/ValuesController.cs
+++ DataManagement/Controllers/ValuesController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Dashboard_DataManagement.Controllers
+namespace DataManagement.Controllers
 {
     [Route("api/[controller]")]
     public class ValuesController : Controller
---
+++ DataManagement/DataManagement.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Fabric;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.ServiceFabric.Services.Communication.AspNetCore;
+using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Microsoft.ServiceFabric.Services.Runtime;
+
+namespace DataManagement
+{
+    /// <summary>
+    /// FabricRuntime crea una instancia de esta clase para cada instancia de tipo de servicio. 
+    /// </summary>
+    internal sealed class DataManagement : StatelessService
+    {
+        public DataManagement(StatelessServiceContext context)
+            : base(context)
+        { }
+
+        /// <summary>
+        /// Reemplazo opcional para crear agentes de escucha (como TCP, HTTP) para esta instancia del servicio.
+        /// </summary>
+        /// <returns>La colección de agentes de escucha.</returns>
+        protected override IEnumerable<ServiceInstanceListener> CreateServiceInstanceListeners()
+        {
+            return new ServiceInstanceListener[]
+            {
+                new ServiceInstanceListener(serviceContext =>
+                    new WebListenerCommunicationListener(serviceContext, "ServiceEndpoint", (url, listener) =>
+                    {
+                        ServiceEventSource.Current.ServiceMessage(serviceContext, $"Starting WebListener on {url}");
+
+                        return new WebHostBuilder().UseWebListener()
+                                    .ConfigureServices(
+                                        services => services
+                                            .AddSingleton<StatelessServiceContext>(serviceContext))
+                                    .UseContentRoot(Directory.GetCurrentDirectory())
+                                    .UseStartup<Startup>()
+                                    .UseApplicationInsights()
+                                    .UseServiceFabricIntegration(listener, ServiceFabricIntegrationOptions.None)
+                                    .UseUrls(url)
+                                    .Build();
+                    }))
+            };
+        }
+    }
+}
